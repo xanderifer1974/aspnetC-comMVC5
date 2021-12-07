4c2222fdba544f54988220c0f93a1a93ed96a22b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-12-07 19:37:43 -0300

    Criando a homeController

--- Introducao/Controllers/HomeController.cs
+++ Introducao/Controllers/HomeController.cs
@@ -8,23 +8,10 @@ namespace Introducao.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // GET: Home
+        public string Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return "Alexandre Ribeiro";
         }
     }
 }
