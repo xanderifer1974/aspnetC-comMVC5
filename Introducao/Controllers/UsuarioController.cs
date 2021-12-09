0ac7b5d82e4cc2c321d5c812794b49d3e81c9fdc
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-12-09 10:25:05 -0300

    Validação com ASP parte 1

---
+++ Introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            Usuario usuario = new Usuario();
+
+            return View(usuario);
+        }
+    }
+}
---
+++ Introducao/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Introducao.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Observacoes { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
