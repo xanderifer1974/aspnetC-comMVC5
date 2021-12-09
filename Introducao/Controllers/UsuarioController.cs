7d75bf54884a83a37264a52871521a06f5cf0a52
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-12-09 10:58:02 -0300

    Classes e Modelos para validação

--- Introducao/Controllers/UsuarioController.cs
+++ Introducao/Controllers/UsuarioController.cs
@@ -5,11 +5,27 @@ namespace Introducao.Controllers
 {
     public class UsuarioController : Controller
     {
-       
+
         public ActionResult Index()
         {
             Usuario usuario = new Usuario();
 
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Resultado(Usuario usuario)
+        {
             return View(usuario);
         }
     }
